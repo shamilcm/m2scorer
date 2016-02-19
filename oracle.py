954729aa72f8b649e267a888dfa9e73bed0cfe8f
Shamil Chollampatt <shamil.cm@gmail.com>
2016-02-19 11:27:12 +0800

    Formatting oracle script and adding evaluation

--- oracle.py
+++ oracle.py
@@ -13,8 +13,9 @@ from scorer.reader import read_nbest_sentences
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input_nbest', dest='input_nbest_path', required=True, help="Path to input nbest file")
 parser.add_argument('-m', '--input_m2file',dest='input_m2_path', required=True, help="Path to input m2 file")
-parser.add_argument('-o', '--output_dir_path', dest='output_dir_path', help="Path to output directory")
+parser.add_argument('-o', '--output_dir_path', dest='output_dir_path', default=".", help="Path to output directory")
 parser.add_argument('-e', '--error_types', dest='error_types', default='all', help="Comma separated error types" )
+parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',  help="Comma separated error types" )
 args = parser.parse_args()
 
 filter_etypes=args.error_types.split(',')
@@ -40,10 +41,16 @@ index = 0
 for nbest,source,golds_set in zip(nbest_sentences,source_sentences, gold_edits):
 	best_candidate_score = 0.0
 	c_index = 0
-	for candidate in nbest:
+
+	if args.verbose is False:
 		print >>sys.stderr, "Sentence Index:" + str(index)
-		print >>sys.stderr, "Source:" + source
-		print >>sys.stderr, "Hypothesis:" + candidate
+		print >>sys.stderr, "Original:" + source
+	
+	for candidate in nbest:
+		if (args.verbose):
+			print >>sys.stderr, "Sentence Index:" + str(index)
+			print >>sys.stderr, "Original:" + source
+			print >>sys.stderr, "Hypothesis:" + candidate
 		score = 0.0
 		ann_score = 0.0
 		for annotator, gold in golds_set.iteritems():
@@ -57,18 +64,19 @@ for nbest,source,golds_set in zip(nbest_sentences,source_sentences, gold_edits):
 			best_candidate = candidate
 			best_candidate_score = score
 		c_index += 1
-		print >> sys.stderr, "Score", score
-	print >> sys.stderr, "best: " + best_candidate + "(score=" + str(best_candidate_score) + ")\n"
+		if (args.verbose):
+			print >> sys.stderr, "Score", score
+	print >> sys.stderr, "Best Candidate: " + best_candidate + "(score=" + str(best_candidate_score) + ")\n"
 	index += 1
 	system_sentences.append(best_candidate)
 
-f=open('output_oracle.txt','wb')
+f=open(args.output_dir_path+'/'+'output_oracle.txt','wb')
 for sentence in system_sentences:
 	f.write(sentence.encode("UTF-8")+'\n')
 f.close()
 
-#p, r, f1 = scripts.levenshtein.batch_multi_pre_rec_f1(system_sentences, source_sentences, gold_edits, max_unchanged_words, beta, ignore_whitespace_casing, verbose, very_verbose)
+p, r, f1 = scorer.levenshtein.batch_multi_pre_rec_f1(system_sentences, source_sentences, gold_edits, max_unchanged_words, beta, ignore_whitespace_casing, verbose, very_verbose)
 
-#print "Precision   : %.4f" % p
-#print "Recall      : %.4f" % r
-#print "F_%.1f       : %.4f" % (beta, f1)
+print "Precision   : %.4f" % p
+print "Recall      : %.4f" % r
+print "F_%.1f       : %.4f" % (beta, f1)
